17a6f2969bf9319eae2431dae2b4f0070650d655
dintonharold87 <dintonharold@gmail.com>
2022-05-05 17:50:20 +0300

    exercise 4

---
+++ exercise4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    /* Write a program that prints out the numeric values of characters Z and z. */
+    char i='Z';
+    char j='z';
+    printf("%d\n",i);
+    printf("%d\n",j);
+    /* Write a program that can print out the numeric value of the newline character (\n). */
+    char k='\n';
+    printf("%d\n",k);
+}
