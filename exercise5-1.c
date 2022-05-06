dffc8d20f0dde5fef2d22da002ea234f7cd56fa6
dintonharold87 <dintonharold@gmail.com>
2022-05-06 13:10:50 +0300

    Write a program to put the characters B, y, and e together on the screen

---
+++ exercise5-1.c
@@ -0,0 +1,13 @@
+/* Write a program to put the characters B, y, and e together on the screen */
+#include <stdio.h>
+int main(){
+    int ch1,ch2,ch3;
+    ch1='B';
+    ch2='Y';
+    ch3='E';
+    putchar(ch1);
+    putchar(ch2);
+    putchar(ch3);
+   
+
+}
