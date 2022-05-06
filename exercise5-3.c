a6809d0f75ad3101177be32223ce9b2436fe0249
dintonharold87 <dintonharold@gmail.com>
2022-05-06 13:27:52 +0300

    Given three integers, 15, 150, and 1500, write a program that prints the integers on the screen in the hex format.

---
+++ exercise5-3.c
@@ -0,0 +1,13 @@
+/* Given three integers, 15, 150, and 1500, write a program that prints the integers on
+the screen in the hex format. */
+#include<stdio.h>
+int main(){
+int a,b,c;
+a=15;
+b=150;
+c=1500;
+printf("The hex format of %d is %x\n",a,a);
+printf("The hex format of %d is %x\n",b,b);
+printf("The hex format of %d is %x\n",c,c);
+return 0;
+}
