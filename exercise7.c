8772352e25c86d856dc4f0dd0c247aeaa74ebf72
dintonharold87 <dintonharold@gmail.com>
2022-05-07 15:29:34 +0300

    A while loop that iterates until the character X is entered

---
+++ exercise7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int c;
+    c = ' ';
+    printf("Enter a character:\n(enter x to exit)\n");
+    while (c != 'x'); {
+        c = getchar();
+        putchar(c);
+    }
+    printf("\nOut of the while loop. Bye!\n");
+    return 0;
+}
